3244f406828535a3db6e36cedeab221b27549188
Kesit Budi Kusumo <kesit@kernel.id>
2024-06-26 23:59:56 +0700

    feat: add iterative solution

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	input := []int{2, 3, 4}
+	target := 6
+	output := twoSum(input, target)
+	fmt.Println(output)
+}
+
+func twoSum(numbers []int, target int) []int {
+	prevsMap := map[int]int{}
+	result := make([]int, 2)
+
+	for i := 0; i < len(numbers); i++ {
+		if idxPrev, ok := prevsMap[target-numbers[i]]; ok {
+			result[0] = idxPrev + 1
+			result[1] = i + 1
+			break
+		}
+		prevsMap[numbers[i]] = i
+	}
+
+	return result
 }
